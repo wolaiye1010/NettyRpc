f59ba469d78771a837b93554f557af7bdb49dd3d
zhudongchang <zhudongchang@58ganji.com>
2019-04-17 10:31:32 +0800

    rpc server

--- src/test/java/com/bj58/huangye/rpc/test/app/Benchmark.java
+++ src/test/java/com/bj58/huangye/rpc/test/app/Benchmark.java
@@ -12,47 +12,16 @@ import org.junit.Assert;
 public class Benchmark {
 
     public static void main(String[] args) throws InterruptedException {
-        ServiceDiscovery serviceDiscovery = RpcInit.builder()
+        RpcInit.builder()
                 .setEnv("offline")
                 .setServiceName("confcenter")
                 .setZkConnectionString("10.126.102.152:3181,10.126.91.8:3181,10.126.91.84:3181")
                 .clientInit();
-        RpcClient rpcClient = new RpcClient(serviceDiscovery);
-
-        HelloService helloService = rpcClient.create(HelloService.class);
+        HelloService helloService = RpcClient.create(HelloService.class);
         String result = helloService.hello("World");
         Assert.assertEquals("Hello! World", result);
 
         System.out.println(result);
         System.exit(0);
-
-        int threadNum = 10;
-        final int requestNum = 100;
-        Thread[] threads = new Thread[threadNum];
-
-        long startTime = System.currentTimeMillis();
-        //benchmark for sync call
-        for (int i = 0; i < threadNum; ++i) {
-            threads[i] = new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    for (int i = 0; i < requestNum; i++) {
-                        final HelloService syncClient = rpcClient.create(HelloService.class);
-                        String result = syncClient.hello(Integer.toString(i));
-                        if (!result.equals("Hello! " + i))
-                            System.out.print("error = " + result);
-                    }
-                }
-            });
-            threads[i].start();
-        }
-        for (int i = 0; i < threads.length; i++) {
-            threads[i].join();
-        }
-        long timeCost = (System.currentTimeMillis() - startTime);
-        String msg = String.format("Sync call total-time-cost:%sms, req/s=%s", timeCost, ((double) (requestNum * threadNum)) / timeCost * 1000);
-        System.out.println(msg);
-
-        rpcClient.stop();
     }
 }
